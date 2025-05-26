10d9b3424b3ea8b4362468b71d89a2942a397928
bijeesh27 <bijeeshbiju3669@gmail.com>
2025-05-26 11:50:04 +0530

    create Append

--- week1/linkedlist.js
+++ week1/linkedlist.js
@@ -13,6 +13,8 @@ class LinkedList{
         this.head=null;
     }
 
+ // Prepend - adding an element to the START of the list
+    
     prepend(value){
         let node = new Node(value)
         if(this.head==null){
@@ -23,6 +25,22 @@ class LinkedList{
         }
     }
 
+// Append - adding an element to the END of the list
+
+    append(value){
+        let node= new Node(value)
+        if(this.head==null){
+            this.head=node
+        }else{
+            let prev=this.head
+            while(prev.next){
+                prev=prev.next
+            }
+            prev.next=node
+        }
+    }
+
+// Print - display the contents of the list
 
     print(){
         if(this.head==null){
@@ -47,5 +65,10 @@ list.prepend(4)
 list.prepend(2)
 list.prepend(6)
 list.prepend(3)
+
+list.append(2)
+list.append(5)
+list.append(8)
+list.append(9)
 
 list.print()
