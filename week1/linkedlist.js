33dad57feb2a4754ed24a2c5b11c45543087c506
bijeesh27 <bijeeshbiju3669@gmail.com>
2025-05-26 12:51:33 +0530

    create Reverse

--- week1/linkedlist.js
+++ week1/linkedlist.js
@@ -1,99 +1,110 @@
 //Linked List
 
-class Node{
-    constructor(value){
-        this.value=value;
-        this.next=null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-class LinkedList{
-    constructor(){
-        this.head=null;
-        this.size=0
+  // Prepend - adding an element to the START of the list
+
+  prepend(value) {
+    let node = new Node(value);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
     }
+    this.size++;
+  }
 
- // Prepend - adding an element to the START of the list
-    
-    prepend(value){
-        let node = new Node(value)
-        if(this.head==null){
-            this.head=node
-        }else{
-            node.next=this.head
-            this.head=node
-        }
-        this.size++
+  // Append - adding an element to the END of the list
+
+  append(value) {
+    let node = new Node(value);
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
     }
+    this.size++;
+  }
 
-// Append - adding an element to the END of the list
+  // Insert - add an element at a specific position in the list
 
-    append(value){
-        let node= new Node(value)
-        if(this.head==null){
-            this.head=node
-        }else{
-            let prev=this.head
-            while(prev.next){
-                prev=prev.next
-            }
-            prev.next=node
-        }
-        this.size++
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      return null;
+    } else if (index === 0) {
+      this.prepend(value);
+    } else {
+      let node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
     }
+  }
 
+  // Reverse - reverse the order of elements in the list
 
-// Insert - add an element at a specific position in the list
-
-    insert(value,index){
-        
-        if(index < 0 || index > this.size){
-            return null
-        }else if(index===0){
-            this.prepend(value)
-        }else{
-            let node =new Node(value)
-            let prev=this.head
-            for(let i=0;i<index-1;i++){
-                prev=prev.next
-            }
-            node.next=prev.next
-            prev.next=node
-            this.size++
-        }
+  reverse() {
+    let prev = null;
+    let curr = this.head;
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
     }
+    this.head = prev;
+  }
 
-// Print - display the contents of the list
+  // Print - display the contents of the list
 
-    print(){
-        if(this.head==null){
-            console.log("The List Is Empty");
-        }else{
-            let curr=this.head;
-            let listValues=''
-            while(curr){
-                listValues+=`${curr.value} -> `
-                curr=curr.next
-            }
-            console.log(listValues)
-        }
+  print() {
+    if (this.head == null) {
+      console.log("The List Is Empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} -> `;
+        curr = curr.next;
+      }
+      console.log(listValues);
     }
-
+  }
 }
 
+let list = new LinkedList();
 
-let list=new LinkedList()
+list.prepend(4);
+list.prepend(2);
+list.prepend(6);
+list.prepend(3);
 
-list.prepend(4)
-list.prepend(2)
-list.prepend(6)
-list.prepend(3)
+list.append(2);
+list.append(5);
+list.append(8);
+list.append(9);
+list.insert(777, 4);
 
-list.append(2)
-list.append(5)
-list.append(8)
-list.append(9)
-list.insert(777,4)
-
-list.print()
+list.print();
+list.reverse();
+list.print();
